9962b6bd17a12bc2e60534bb3fed75b27ecc80e1
Steve Behm <stevtbehm1@gmail.com>
2022-06-01 03:16:41 +0000

    completed color flipper vanilla js project.

---
+++ javascript-color-flipper/color-flipper.js
@@ -0,0 +1,22 @@
+const colors = [
+  'green',
+  'red',
+  'rgba(133,122,200)',
+  '#f15025'
+];
+
+const $btn = document.getElementById('btn');
+const $color = document.querySelector('.color')
+
+$btn.addEventListener('click', function() {
+  // get random number between 0 and 3, which is the available indexes in the array
+  const randomNumber = Math.floor(getRandomNumber());
+  console.log(randomNumber)
+
+  document.body.style.backgroundColor = colors[randomNumber];
+  $color.textContent = colors[randomNumber];
+})
+
+function getRandomNumber() {
+  return Math.random() * colors.length;
+}
