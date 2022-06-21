9b7a6d20253509ab9b50557e82654bfe3c0a9bd3
Steve Behm <stevtbehm1@gmail.com>
2022-06-21 19:50:44 +0000

    completed ability to search for item and bring focus back to add item element.

--- react-tutorial-playlist/src/App.js
+++ react-tutorial-playlist/src/App.js
@@ -58,7 +58,7 @@ function App() {
         setSearch={setSearch}
       />
       <Content
-        items={items}
+        items={items.filter(item => ((item.item).toLowerCase()).includes(search.toLowerCase()))}
         handleCheck={handleCheck}
         handleDelete={handleDelete}
       />
