688acaeea8bb48bbc6c15a9f682a1f03dc47b8b0
Steve Behm <stevtbehm1@gmail.com>
2022-06-23 17:13:03 +0000

    completed part 1 of the fetch api challenge.

--- react-tutorial-playlist/src/App.js
+++ react-tutorial-playlist/src/App.js
@@ -4,14 +4,14 @@ import Footer from "./components/footer";
 import AddItem from "./components/AddItem";
 import SearchItem from "./components/SearchItem";
 import apiRequest from "./components/apiRequest";
-import { useState, useEffect } from 'react'
+import { useState, useEffect } from "react";
 
 function App() {
-  const API_URL = 'http://localhost:3001/items';
+  const API_URL = "http://localhost:3001/items";
 
   const [items, setItems] = useState([]);
-  const [newItem, setNewItem] = useState('');
-  const [search, setSearch] = useState('');
+  const [newItem, setNewItem] = useState("");
+  const [search, setSearch] = useState("");
   const [fetchError, setFetchError] = useState(null);
   const [isLoading, setIsLoading] = useState(true);
 
@@ -21,7 +21,7 @@ function App() {
       try {
         const response = await fetch(API_URL);
         if (!response.ok) {
-          throw Error('Did not receive expected data')
+          throw Error("Did not receive expected data");
         }
         const listItems = await response.json();
 
@@ -29,16 +29,15 @@ function App() {
         setFetchError(null);
       } catch (err) {
         setFetchError(err.message);
-
       } finally {
         setIsLoading(false);
       }
-    }
+    };
     // simulate a delay in loading the data from an API
     setTimeout(() => {
       fetchItems();
-    }, 2000)
-  }, [])
+    }, 2000);
+  }, []);
 
   const addItem = async (item) => {
     // if there are items, find the last value in the array and increment its id
@@ -49,84 +48,87 @@ function App() {
     setItems(listItems);
 
     const postOptions = {
-      method: 'POST',
+      method: "POST",
       headers: {
-        'Content-Type': 'application/json'
+        "Content-Type": "application/json",
       },
-      body: JSON.stringify(myNewItem)
-    }
+      body: JSON.stringify(myNewItem),
+    };
     // result will return null if there is no error
     // if result is truthy that means we have an error message and
     // we need to update the fetchError in state
     const result = await apiRequest(API_URL, postOptions);
-    if (result) setFetchError(result)
-  }
+    if (result) setFetchError(result);
+  };
 
   const handleCheck = async (id) => {
-    const listItems = items.map(item => item.id === id ? { ...item, checked: !item.checked} : item);
+    const listItems = items.map((item) =>
+      item.id === id ? { ...item, checked: !item.checked } : item
+    );
     setItems(listItems);
 
     // update a list item
-    const myItem = listItems.filter(item => item.id === id);
+    const myItem = listItems.filter((item) => item.id === id);
     const updateOptions = {
-      method: 'PATCH',
+      method: "PATCH",
       headers: {
-        'Content-Type': 'application/json'
+        "Content-Type": "application/json",
       },
       // filter returns an array so we need to use myItem[0]
-      body: JSON.stringify({ checked: myItem[0].checked })
-    }
+      body: JSON.stringify({ checked: myItem[0].checked }),
+    };
     // define the request URL since it is slightly different when updating
     const reqUrl = `${API_URL}/${id}`;
     // define our result
-    const result = await apiRequest(reqUrl, updateOptions)
-    if (result) setFetchError(result)
-  }
+    const result = await apiRequest(reqUrl, updateOptions);
+    if (result) setFetchError(result);
+  };
 
   const handleDelete = async (id) => {
-    const listItems = items.filter(item => item.id !== id)
+    const listItems = items.filter((item) => item.id !== id);
     setItems(listItems);
 
     // delete an item
     const deleteOptions = {
-      method: 'DELETE'
+      method: "DELETE",
     };
     const reqUrl = `${API_URL}/${id}`;
     const result = await apiRequest(reqUrl, deleteOptions);
-    if (result) setFetchError(result)
-  }
+    if (result) setFetchError(result);
+  };
 
   const handleSubmit = (e) => {
     e.preventDefault();
 
-    if(!newItem) return;
+    if (!newItem) return;
 
     // addItem
-    addItem(newItem)
+    addItem(newItem);
     // remove item from input value
-    setNewItem('');
-  }
+    setNewItem("");
+  };
 
   return (
-    <div className='App'>
+    <div className="App">
       <Header title="Grocery List" />
       <AddItem
         newItem={newItem}
         setNewItem={setNewItem}
         handleSubmit={handleSubmit}
       />
-      <SearchItem
-        search={search}
-        setSearch={setSearch}
-      />
+      <SearchItem search={search} setSearch={setSearch} />
       <main>
         {isLoading && <p>Loading Items...</p>}
-        {fetchError && <p style={{ color: 'red' }}>{`Error: ${fetchError}`}</p>}
-        {!fetchError && !isLoading && <Content
-          items={items.filter(item => ((item.item).toLowerCase()).includes(search.toLowerCase()))}
-          handleCheck={handleCheck}
-          handleDelete={handleDelete}
-        />}
+        {fetchError && <p style={{ color: "red" }}>{`Error: ${fetchError}`}</p>}
+        {!fetchError && !isLoading && (
+          <Content
+            items={items.filter((item) =>
+              item.item.toLowerCase().includes(search.toLowerCase())
+            )}
+            handleCheck={handleCheck}
+            handleDelete={handleDelete}
+          />
+        )}
       </main>
       <Footer length={items.length} />
     </div>
